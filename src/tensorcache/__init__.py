9633a90715a0ce5bc6eca1d540cd1be4e6ebf98a
Thenukegun10x <thenukegun10x@github.com>
2026-09-22 08:47:48 +0800

    Release 0.5.0: open_append grows feature caches in place

--- src/tensorcache/__init__.py
+++ src/tensorcache/__init__.py
@@ -75,7 +75,7 @@ from .pixel_cache import (
 )
 from .streamer import ZeroCopyTensorStreamer
 
-__version__ = "0.4.3"
+__version__ = "0.5.0"
 __all__ = [
     "BlockwiseInt8Codec",
     "quantize_int8_g32",
